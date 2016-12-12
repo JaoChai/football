580b1b5ea65abc12d90c043ad955219d5d19fc93
iMaew <imaew@iMaew.local>
2016-12-12 11:29:09 +0700

    Football

---
+++ application/views/backend/football.php
@@ -0,0 +1,81 @@
+<div id="wrapper">
+
+       <div id="page-wrapper">
+
+           <div class="container-fluid">
+
+               <!-- Page Heading -->
+               <div class="row">
+                   <div class="col-lg-12">
+                       <h1 class="page-header">
+                           จัดการตารางบอล
+                       </h1>
+                   </div>
+               </div>
+               <!-- /.row -->
+               <div class="row">
+                    <div class="col-lg-6">
+
+                        <form role="form">
+
+                          <div class="form-group">
+                            <label>เลือกลีก</label>
+                            <select class="form-control">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                          </div>
+
+                          <div class="form-group">
+                          <label>เลือกช่อง</label>
+                            <select class="form-control">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                          </div>
+
+                          <div class="form-group">
+                              <label>เวลา</label>
+                              <input class="form-control">
+                          </div>
+
+                            <div class="form-group">
+                                <label>ทีม 1</label>
+                                <input class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <label>ทีม 2</label>
+                                <input class="form-control">
+                            </div>
+
+                             <button type="button" class="btn btn-primary" style="float: right;">บันทึก</button>
+
+                            </div>
+
+
+                            <div class="col-lg-6">
+
+
+
+                            </div>
+
+
+                      </div>
+
+                    </div>
+
+           </div>
+           <!-- /.container-fluid -->
+
+       </div>
+       <!-- /#page-wrapper -->
+
+   </div>
+   <!-- /#wrapper -->
