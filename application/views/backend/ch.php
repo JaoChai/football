af511bfe37ac2d8dbfee66ab07048ff7c6cd36bd
iMaew <imaew@iMaew.local>
2016-12-12 11:35:29 +0700

    tv

---
+++ application/views/backend/ch.php
@@ -0,0 +1,46 @@
+<div id="wrapper">
+
+       <div id="page-wrapper">
+
+           <div class="container-fluid">
+
+               <!-- Page Heading -->
+               <div class="row">
+                   <div class="col-lg-12">
+                       <h1 class="page-header">
+                           จัดการช่องทีวี
+                       </h1>
+                   </div>
+               </div>
+               <!-- /.row -->
+               <div class="row">
+                    <div class="col-lg-6">
+
+                        <form role="form">
+
+                          <div class="form-group">
+                              <label>ชื่อช่อง</label>
+                              <input class="form-control">
+                          </div>
+
+                          <div class="form-group">
+                              <label>URL Video</label>
+                              <input class="form-control">
+                          </div>
+
+                           <button type="button" class="btn btn-primary" style="float: right;">บันทึก</button>
+
+                      </div>
+
+                    </div>
+
+            </div>
+
+           </div>
+           <!-- /.container-fluid -->
+
+       </div>
+       <!-- /#page-wrapper -->
+
+   </div>
+   <!-- /#wrapper -->
