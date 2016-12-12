8cf7f139da104a65e189b4e8c0f4305312afd2ce
iMaew <imaew@iMaew.local>
2016-12-12 11:06:58 +0700

    league

--- application/controllers/Backend.php
+++ application/controllers/Backend.php
@@ -11,9 +11,6 @@ class Backend extends CI_Controller {
 	}
 
 
-
-
-
   /* View Backend */
   public function viewmanage(){
     $this->load->view('layoutbackend/header');
@@ -22,9 +19,69 @@ class Backend extends CI_Controller {
     $this->load->view('layoutbackend/footer');
   }
 
+	public function viewleague(){
+		$this->load->view('layoutbackend/header');
+		$this->load->view('layoutbackend/navbar');
+		$this->load->view('backend/league');
+		$this->load->view('layoutbackend/footer');
+	}
+
+	public function viewtablefooball(){
+		$this->load->view('layoutbackend/header');
+		$this->load->view('layoutbackend/navbar');
+		$this->load->view('backend/football');
+		$this->load->view('layoutbackend/footer');
+	}
+
+	public function viewch(){
+		$this->load->view('layoutbackend/header');
+		$this->load->view('layoutbackend/navbar');
+		$this->load->view('backend/ch');
+		$this->load->view('layoutbackend/footer');
+	}
+
+  /* Crud viewleague */
+
+	public function createleague(){
+
+	}
+
+	public function updateleague(){
+
+	}
+
+	public function deleteleague(){
+
+	}
+
+	/* Crud viewfooball */
+
+	public function createfooball()
+	{
+
+	}
+
+	public function updatefooball(){
+
+	}
+
+	public function deletefooball(){
+
+	}
 
 
+	/* Crud viewch */
 
-  /* Crud */
+	public function createch(){
+
+	}
+
+	public function updatech(){
+
+	}
+
+	public function deletech(){
+
+	}
 
 }
---
+++ application/views/backend/league.php
@@ -0,0 +1,51 @@
+<div id="wrapper">
+
+       <div id="page-wrapper">
+
+           <div class="container-fluid">
+
+               <!-- Page Heading -->
+               <div class="row">
+                   <div class="col-lg-12">
+                       <h1 class="page-header">
+                           จัดการลีก
+                       </h1>
+                   </div>
+               </div>
+               <!-- /.row -->
+               <div class="row">
+                    <div class="col-lg-6">
+
+                        <form role="form">
+
+                            <div class="form-group">
+                                <label>เพิ่ม ลีก</label>
+                                <input class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <button type="button" class="btn btn-primary" style="float: right;">บันทึก</button>
+                            </div>
+
+                      </div>
+
+
+                      <div class="col-lg-6">
+
+                        <table class="table table-bordered">
+
+
+                        </table>
+
+                        </div>
+
+                    </div>
+
+           </div>
+           <!-- /.container-fluid -->
+
+       </div>
+       <!-- /#page-wrapper -->
+
+   </div>
+   <!-- /#wrapper -->
