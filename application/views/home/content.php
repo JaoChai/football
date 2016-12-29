736a18a25c08a2ab62ac889c8001aa592e18212c
anugool tippon <anugooltippon@gmail.com>
2016-12-29 14:51:44 +0700

    Test

--- application/views/home/content.php
+++ application/views/home/content.php
@@ -2,7 +2,7 @@
 <br class="cr" />
 		<div class="space30"></div>
 	<div class="">
-		<div style="position:absolute; z-index:10; margin:0px 0px 0px 140px;">  <a href="https://casino1988.net/&url=มาดูบอล.com" target="_blank"><img src="<?php echo base_url(); ?>assets/images/topv.png" alt="มาดูบอล.com" /></a></div>
+		<div style="position:absolute; z-index:10; margin:0px 0px 0px 140px;">  <a href="https://casino1988.net/&url=มาดูบอล.com" target="_blank"><img src="<?php echo base_url(); ?>assets/images/top.gif" alt="มาดูบอล.com" /></a></div>
 		<div style="position:absolute; z-index:10; margin:550px 100px 100px 720px;"><img src="<?php echo base_url(); ?>assets/images/tvlogo.png" alt="มาดูบอล.com" /></div>
 		<div style="position:relative; z-index:5;">
       <iframe frameborder="0" marginwidth="0" marginheight="0" scrolling="no" width="730" height="650" style="top:0px; position:relative;" src="<?php echo $content->ch_url;?>" allowfullscreen="allowfullscreen"></iframe>
