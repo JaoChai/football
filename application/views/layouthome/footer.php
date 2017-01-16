4ee9fa7dcb02b91c0e829c0f44a1d9bb4b290459
anugool tippon <anugooltippon@gmail.com>
2017-01-16 13:56:21 +0700

    Add New Ch

--- application/views/layouthome/footer.php
+++ application/views/layouthome/footer.php
@@ -1,8 +1,24 @@
 <br class="cr" />
-	<div class="space20"></div>
-	<div align="center"><strong>มาดูบอล.com  ดูบอลยูโร 2016 สดๆ ผ่านเน็ตฟรีๆ ไม่ต้องใช้โปรแกรม ทั่วโลก</strong></div>
-	<div class="space100"></div>
+        <div class="space20"></div>
+        <div align="center"><strong>มาดูบอล.com ดูบอลสด ดูบอลออนไลน์ ดูบอลผ่านมือถือ ทุกลีกทั่วโลกฟรี</strong></div><br/>
+        <div align="center">
+        <!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
+<!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,3699088,4,205,255,27,00011111']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?3699088&101" alt="hit tracker" border="0"></a></noscript>
+<!-- Histats.com  END  -->
+        </div>
+        <div class="space100"></div>
 </div>
 
 </body>
 </html>
+~
