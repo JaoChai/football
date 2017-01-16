cb05dffa738428bec7c0b7d2e89e6da6aeb5241d
anugool tippon <anugooltippon@gmail.com>
2017-01-16 14:45:41 +0700

    Add On

--- application/views/layoutbackend/navbar.php
+++ application/views/layoutbackend/navbar.php
@@ -1,7 +1,5 @@
-<!-- Navigation -->
 <?php $session_data = $this->session->userdata('logged_in');?>
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
             <span class="sr-only">Toggle navigation</span>
@@ -9,13 +7,12 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="index.html">Admin Madoball</a>
+        <a class="navbar-brand" href="#">Admin Madoball</a>
     </div>
-    <!-- Top Menu Items -->
     <ul class="nav navbar-right top-nav">
 
       <li class="dropdown">
-          <a target="_blank" href="<?php echo site_url('welcome/index');?>" > Go To Web </a>
+          <a target="_blank" href="<?php echo base_url();?>" > Go To Web </a>
       </li>
 
         <li class="dropdown">
